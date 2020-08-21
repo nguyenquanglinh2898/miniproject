66e76310eae483d1ed91e3bdec4a844efcfd223c
quydang <hadesi1999@gmail.com>
2020-08-21 10:38:08 +0700

    Refactor code in index.php

--- index.php
+++ index.php
@@ -1,13 +1,31 @@
 <?php
-  include_once('controllers/BookController.php');
+  // include_once('controllers/BookController.php');
 
-  $bookController = new BookController();
+  // $bookController = new BookController();
   
-  if($_GET['controller'] == 'book' && $_GET['action'] == 'getAll') $bookController->getAll();
+  // if($_GET['controller'] == 'book' && $_GET['action'] == 'getAll') $bookController->getAll();
 
-  if($_GET['controller'] == 'book' && $_GET['action'] == 'add') $bookController->add();
+  // if($_GET['controller'] == 'book' && $_GET['action'] == 'add') $bookController->add();
 
-  if($_GET['controller'] == 'book' && $_GET['action'] == 'edit') $bookController->edit($_GET['id']);
+  // if($_GET['controller'] == 'book' && $_GET['action'] == 'edit') $bookController->edit($_GET['id']);
 
-  if($_GET['controller'] == 'book' && $_GET['action'] == 'update') $bookController->update($_GET['id']);
+  // if($_GET['controller'] == 'book' && $_GET['action'] == 'update') $bookController->update($_GET['id']);
+
+  if($_GET['controller'] == 'book'){
+    require_once('controllers/BookController.php');
+    $bookController = new BookController();
+    switch($_GET['action']){
+      case 'add':
+        $bookController->add();
+        break;
+      case 'edit':
+        $bookController->edit($_GET['id']);
+        break;
+      case 'update':
+        $bookController->update($_GET['id']);
+        break;
+      default:
+        $bookController->getAll();
+      }
+  }
 ?>
